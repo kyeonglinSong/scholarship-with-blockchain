73e0349b17b787bc204b2b5d2239e3543ebdd19c
Eunbyeol <kebinkorea@gmail.com>
2020-02-17 15:53:42 +0900

    solved issues on LogIn page grid

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,13 +3,12 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import "./content.css"
 import styled from 'styled-components';
 import LOGO from '../images/logo.PNG'
-
+import './content.css';
 const SignIn = ({form, onChange, onSubmit})=>{
 
   const inputStyle = {
-    width:'37.5%',
-    marginLeft:'auto',
-    marginRight: 'auto'
+    width:'33.3%',
+    margin:'auto'
   }
 
   return(
@@ -18,8 +17,8 @@ const SignIn = ({form, onChange, onSubmit})=>{
     <div style={{overflowX: 'hidden'}}>
       <br/><br/><br/>
       <div style={{textAlign: 'center'}}><img className="center" src={LOGO} style={{width: '150px'}}/><br/></div>
-      <span className="content">
-        <Form onSubmit={onSubmit} style={{marginLeft:'auto', marginRight:'auto'}}>
+      <span className="content2">
+        <Form onSubmit={onSubmit} >
           <FormGroup>
             <Label for="exampleEmail">Email</Label>
             <Input style={inputStyle} type="email" name="email" id="exampleEmail" placeholder="학번"
