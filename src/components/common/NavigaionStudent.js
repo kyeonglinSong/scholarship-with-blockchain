bb1b44facec5a0ef5255453324eec5bdd1ce6291
Eunbyeol <kebinkorea@gmail.com>
2020-02-17 18:53:50 +0900

    DECO

--- src/components/common/NavigaionStudent.js
+++ src/components/common/NavigaionStudent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, NavbarBrand } from "reactstrap"
+import {Navbar, Nav, NavLink, NavbarBrand } from "reactstrap"
 import { Link } from 'react-router-dom';
 import LOGO from '../../images/logo.PNG'
 
@@ -15,7 +15,7 @@ const NavigationStudent = ( {user})=>{
   }
 
   const brandStyle={
-    color:'#ffffff',
+    textColor:'black',
   }
   
   return(
@@ -23,28 +23,30 @@ const NavigationStudent = ( {user})=>{
       <HeaderContainer />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <br/>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavbarBrand className="nav-link mr-auto" style={brandStyle}><NavLink href="/student"><img src={LOGO} style={iconStyle}/>학생용장학금관리</NavLink></NavbarBrand>
+      <Navbar color="light" light expand="md" className="mr-auto navbar navbar-expand-lg navbar-light bg-white">
+      
+        <NavbarBrand style={{fontSize: 'x-large'}} className="nav-link ml-5 black" ><NavLink href="/student"><img src={LOGO} style={iconStyle}/>학생용장학금관리</NavLink></NavbarBrand>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className="nav-item" style={{ marginLeft:'20px'}}>
+            <ul className="navbar-nav mt-2 mt-lg-0">
+                <li className="nav-item" style={{ marginLeft:'100px'}}>
                     <Link className="nav-link" to="/notices">공지사항</Link>
                 </li>
-                <li className="nav-item" style={{ marginLeft:'20px'}}>
+                <li className="nav-item" style={{ marginLeft:'100px'}}>
                     <Link className="nav-link" to="/scholars">장학금 정보/신청</Link>
                 </li>
-                <li className="nav-item" style={{ marginLeft:'20px'}}>
+                <li className="nav-item" style={{ marginLeft:'100px'}}>
                     <Link className="nav-link" to="/applies">신청현황조회</Link>
                 </li>
-                <li className="nav-item" style={{ marginLeft:'20px'}}>
+                <li className="nav-item" style={{ marginLeft:'100px'}}>
                     <Link className="nav-link" to="/student">사이트이용방법</Link>
                 </li>
             </ul>
         </div>
-      </nav>
+        
+      </Navbar>
     </div>
   );
 }
