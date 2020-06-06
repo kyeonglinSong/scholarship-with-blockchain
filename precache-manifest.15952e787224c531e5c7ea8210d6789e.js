109afa16d4d486f093669080e16304527c8d96e3
Kyeonglin <blossommilktea@gmail.com>
2020-06-06 13:52:15 +0900

    Updates

---
+++ precache-manifest.15952e787224c531e5c7ea8210d6789e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377c5c1c152057e5f0f8c7161036ce8d",
+    "url": "/scholarship-with-blockchain/index.html"
+  },
+  {
+    "revision": "71b4651231e4d842cc9f",
+    "url": "/scholarship-with-blockchain/static/css/2.b8f9ecc0.chunk.css"
+  },
+  {
+    "revision": "92819d927fae3ac4b557",
+    "url": "/scholarship-with-blockchain/static/css/main.bf41504e.chunk.css"
+  },
+  {
+    "revision": "71b4651231e4d842cc9f",
+    "url": "/scholarship-with-blockchain/static/js/2.abb36fb3.chunk.js"
+  },
+  {
+    "revision": "a9ade650d459cafa5d048009c3f4e90c",
+    "url": "/scholarship-with-blockchain/static/js/2.abb36fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92819d927fae3ac4b557",
+    "url": "/scholarship-with-blockchain/static/js/main.04c3510c.chunk.js"
+  },
+  {
+    "revision": "0a58021645a80ccce90f",
+    "url": "/scholarship-with-blockchain/static/js/runtime-main.bf5d5735.js"
+  },
+  {
+    "revision": "3ab8fb0de1108da8ad81d3a52e7d4016",
+    "url": "/scholarship-with-blockchain/static/media/background.3ab8fb0d.png"
+  },
+  {
+    "revision": "f0d41bc25f4a5e3b92587ca5a665150c",
+    "url": "/scholarship-with-blockchain/static/media/background_student.f0d41bc2.png"
+  },
+  {
+    "revision": "63df473f884f810ac662e2f227f01e3f",
+    "url": "/scholarship-with-blockchain/static/media/examplepic01.63df473f.png"
+  },
+  {
+    "revision": "c83e1bb8f261ea8bcfac79a70e767c79",
+    "url": "/scholarship-with-blockchain/static/media/logo.c83e1bb8.PNG"
+  }
+]);
